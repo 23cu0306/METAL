4785a9297824058896867c9c8dda4f9a2f952f94
DESKTOP-LKKHAGK\student <23CU0114@jec.ac.jp>
2025-05-16 16:46:56 +0900

    gamemanager

--- Assets/Script/MCamera.cs
+++ Assets/Script/MCamera.cs
@@ -5,82 +5,88 @@ using UnityEngine;
 
 public class MCamera : MonoBehaviour
 {
-	//プレイヤーの情報（Inspecterで設定)
-	public GameObject player;
+    //プレイヤーの情報（Inspecterで設定)
+    public GameObject player;
+    GameObject _en;
 
-	
-	//追従するかしないか
-	bool IsHorming;
-	//x座標更新地
-	float MostPosX;
-	//現在のX座標
-	float PosX;
+    [SerializeField] Renderer _r;
 
-	
-	public Transform cameraTransform; // カメラのTransform
-	
-	Player _pl;
-	
-	public bool startZoomOut = false;   // ズームアウトを開始するかどうか
-   
+    //追従するかしないか
+    bool IsHorming;
+    //x座標更新地
+    float MostPosX;
+    //現在のX座標
+    float PosX;
+
+
+    public Transform cameraTransform; // カメラのTransform
+
+    Player _pl;
+
+    public bool startZoomOut = false;   // ズームアウトを開始するかどうか
+
     public float moveSpeed = 2f;        // カメラの移動速度
 
-	private Vector3 targetPosition;
-	// Start is called before the first frame update
-	void Start()
-	{
-		_pl = GameObject.Find("Player 1").GetComponent<Player>();
+    private Vector3 targetPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _pl = GameObject.Find("Player").GetComponent<Player>();
+        _en = GameObject.Find("R!_0");
+        _r = _en.GetComponent<SpriteRenderer>();
         IsHorming = true;
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (_en != null)
+        {
+            StopHorming();
+        }
 
-		
-	}
-
-	// Update is called once per frame
-	void Update()
-	{
-		StopHorming();
         MostPos();
         GetPos();
 
-
         PlayerHorming(IsHorming);
+    }
 
-		
 
-		
-	}
 
-	
-    
 
     //追従
-    void PlayerHorming (bool Frg)
-	{
-		if(Frg)
-		{
-			//x座標だけプレイヤーの位置に合わせる
-			transform.position = new Vector3(GetPos(), 0.0f, transform.position.z);
-		}
-	}
+    void PlayerHorming(bool Frg)
+    {
+        if (Frg)
+        {
+            //x座標だけプレイヤーの位置に合わせる
+            transform.position = new Vector3(GetPos(), 0.0f, transform.position.z);
+        }
+    }
 
-	void StopHorming()
-	{
-		if(GetPos() >= 55)
-		{
-			IsHorming = false;
-		}
+    void StopHorming()
+    {
+        if (IsVisible())
+        {
+            Debug.Log("敵いる");
+            IsHorming = false;
+        }
 
-		else if(MostPos() > GetPos())
-		{
-			IsHorming = false;
-		}
 
-		else if (!(MostPos() > GetPos()))
-		{
-			IsHorming = true;
-		}
-	}
+        else if (MostPos() > GetPos())
+        {
+            IsHorming = false;
+        }
+
+        if (!(MostPos() > GetPos()) || !IsVisible())
+        {
+            IsHorming = true;
+
+            Debug.Log("敵いない");
+        }
+
+
+    }
 
     float MostPos()
     {
@@ -99,5 +105,10 @@ public class MCamera : MonoBehaviour
         return PosX;
     }
 
+    public bool IsVisible()
+    {
+        return _r.isVisible;
 
+        Debug.Log(_r.isVisible);
+    }
 }
