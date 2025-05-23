4753122b8eab729c0ff6a9bca69f6b07d9dafb29
DESKTOP-LKKHAGK\student <>
2025-05-23 15:32:11 +0900

    グレネード途中

--- Assets/Script/Player/bomb.cs
+++ Assets/Script/Player/bomb.cs
@@ -2,31 +2,32 @@ using UnityEngine;
 
 public class bomb : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
+    public float throwForce = 10f;
     public float explosionDelay = 2f;
-    public GameObject explosionEffect;
-    public float explosionRadius = 2f;
+    public float explosionRadius = 3f;
     public float damage = 50f;
+    public GameObject explosionEffect;
+    public static int activeGrenadeCount = 0;
 
     private Rigidbody2D rb;
 
     void Start()
     {
+        int playerLayer = LayerMask.NameToLayer("Player");
+        int bombLayer = LayerMask.NameToLayer("Bullet");
+        Physics2D.IgnoreLayerCollision(playerLayer, bombLayer, true);
+        activeGrenadeCount++;
         rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.right * throwForce + transform.up * (throwForce / 2), ForceMode2D.Impulse);
         Invoke("Explode", explosionDelay);
     }
 
     void Explode()
     {
-        // 爆発エフェクトを生成
-        if (explosionEffect != null)
-        {
-            Instantiate(explosionEffect, transform.position, Quaternion.identity);
-        }
+        // 爆発エフェクト
+       // Instantiate(explosionEffect, transform.position, transform.rotation);
 
-        // 周囲の敵を検出してダメージを与える
-        Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
+      Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
         foreach (Collider2D col in enemies)
         {
             if (col.CompareTag("Enemy"))
@@ -34,12 +35,18 @@ public class bomb : MonoBehaviour
                 // 敵にダメージを与える処理（Enemyスクリプトを呼ぶなど）
                 col.GetComponent<GloomVisBoss>()?.TakeDamage(damage);
                 col.GetComponent<ScarletClawBoss>()?.TakeDamage((int)damage);
-                //col.GetComponent<Enemy_Jump>()?.TakeDamage(damage);
+                col.GetComponent<Enemy_Jump>()?.TakeDamage(damage);
             }
         }
 
+        activeGrenadeCount--;
         // グレネードを削除
         Destroy(gameObject);
 
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
