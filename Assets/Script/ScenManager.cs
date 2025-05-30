126d75ed06b83d80908fc6a0ac0bc8ab7f46a654
DESKTOP-LKKHAGK\student <>
2025-05-30 10:46:53 +0900

    sceanの切り替えをコントローラーでできるようにしました。

--- Assets/Script/ScenManager.cs
+++ Assets/Script/ScenManager.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+
 
 public class ScenManager : MonoBehaviour
 {
+    public PlayerControls playerControls;
+
     public string SceneName = "main";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -10,10 +16,21 @@ public class ScenManager : MonoBehaviour
         
     }
 
+    void Awake()
+    {
+        playerControls = new PlayerControls(); 
+    }
+
+    void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Z))
+        if(playerControls.Player.Attack.triggered)
         {
             // シーン切り替えでゲームオーバーを演出
             SceneManager.LoadScene(SceneName);
