459646cd53c914419f4aefc346ceadcbb70163b6
881PC-02\student <>
2025-07-29 15:23:52 +0900

    UI変更

---
+++ Assets/Script/Scene/BombUI.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BombUI : MonoBehaviour
+{
+    public gurenerd bomb;   //ボスのHP確認用
+    public Text bombText;		//BossのHP表示
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlayerHP();
+    }
+    private void UpdatePlayerHP()
+    {
+        if (bomb != null && bombText != null)
+        {
+            bombText.text = $"BOMB\n {bomb.MAXBomb}";
+        }
+    }
+}
